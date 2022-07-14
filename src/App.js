6e065d31b243c7ad2c0bfba0f48f2e9f67cf09a5
Jakub Levanszky <j.levanszky@gmail.com>
2022-07-14 18:27:26 +0200

    chore: basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="spender-app">
       <h1>Spender</h1>
     </div>
   );
-}
+};
 
 export default App;
