292123515314367a048c0db9c6269881f08382d7
Jakub Levanszky <j.levanszky@gmail.com>
2022-08-06 20:17:16 +0200

    chore: font imported from google fonts

--- src/features/ui/theme/global.js
+++ src/features/ui/theme/global.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import { palette } from './palette';
+import { font } from './typograhpy';
 
 export const GlobalStyle = createGlobalStyle`
 *, *::before, *::after {
@@ -15,5 +16,9 @@ html, body {
 
 html {
   font-size: 62.5%; // 16 px = 1.6 rem | cons = if there is some library imported which uses etc 1 rem as a unit that will cause a problem and in whole application
+}
+
+body {
+  font: 400 1.6rem ${font.base}
 }
 `;
