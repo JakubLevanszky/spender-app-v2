d0743813b8c921c91d2f23c6d90fc6ffe736a940
Jakub Levanszky <j.levanszky@gmail.com>
2022-11-27 00:39:36 +0100

    chore: change of colour from palette

--- src/features/ui/components/ValueSpan/ValueSpan.styled.js
+++ src/features/ui/components/ValueSpan/ValueSpan.styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { palette } from '../../theme/palette';
 
 export const ValueSpan = styled.span`
   font-weight: bold;
-  color: ${(props) => (props.negative ? 'red' : '')};
+  color: ${(props) => (props.negative ? palette.red : '')};
 `;
